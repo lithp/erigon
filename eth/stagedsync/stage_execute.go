da8a218fbc984430d931620d342c82326fd43203
alex.sharov <AskAlexSharov@gmail.com>
2021-03-11 15:50:41 +0700

    add execution support

--- eth/stagedsync/stage_execute.go
+++ eth/stagedsync/stage_execute.go
@@ -422,16 +422,16 @@ func UnwindExecutionStage(u *UnwindState, s *StageState, stateDB ethdb.Database,
 			if err := writeAccountPlain(logPrefix, tx, key, acc); err != nil {
 				return err
 			}
-			if params.Cache != nil {
-				params.Cache.SetAccountWrite([]byte(key), &acc)
-			}
+			//if params.Cache != nil {
+			//	params.Cache.SetAccountWrite([]byte(key), &acc)
+			//}
 		} else {
 			if err := deleteAccountPlain(tx, key); err != nil {
 				return err
 			}
-			if params.Cache != nil {
-				params.Cache.SetAccountDelete([]byte(key))
-			}
+			//if params.Cache != nil {
+			//	params.Cache.SetAccountDelete([]byte(key))
+			//}
 		}
 	}
 
@@ -441,16 +441,16 @@ func UnwindExecutionStage(u *UnwindState, s *StageState, stateDB ethdb.Database,
 			if err := tx.Put(stateBucket, k[:storageKeyLength], value); err != nil {
 				return err
 			}
-			if params.Cache != nil {
-				params.Cache.SetStorageWrite(k[:20], binary.BigEndian.Uint64(k[20:28]), k[28:], value)
-			}
+			//if params.Cache != nil {
+			//	params.Cache.SetStorageWrite(k[:20], binary.BigEndian.Uint64(k[20:28]), k[28:], value)
+			//}
 		} else {
 			if err := tx.Delete(stateBucket, k[:storageKeyLength], nil); err != nil {
 				return err
 			}
-			if params.Cache != nil {
-				params.Cache.SetStorageDelete(k[:20], binary.BigEndian.Uint64(k[20:28]), k[28:])
-			}
+			//if params.Cache != nil {
+			//	params.Cache.SetStorageDelete(k[:20], binary.BigEndian.Uint64(k[20:28]), k[28:])
+			//}
 		}
 	}
 
